20c0bade6777c30fd0c965d4b2cc9c4334b61d82
castrorj <castrorj@ter.teradyne.com>
2017-10-27 10:31:09 -0600

    Forgot Pass actualizado

--- src/Controller/ForgotPassController.java
+++ src/Controller/ForgotPassController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controller;
 
 import java.net.URL;
@@ -10,34 +6,28 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javax.swing.JOptionPane;
 
-/**
- * FXML Controller class
- *
- * @author jonathancastro
- */
 public class ForgotPassController implements Initializable {
 
     Model.Conection conn = new Model.Conection();
     CallableStatement stmt;
     ResultSet rs;
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    Connection cn = conn.conexion();
     
     @FXML
-    private ComboBox cmb_Question;
+    public ComboBox<String> cmb_Question;
     @FXML
     private TextField txt_answer;
     @FXML
@@ -52,44 +42,76 @@ public class ForgotPassController implements Initializable {
     private Label lbl_confirmP;
     @FXML
     private Button btn_save;
+    @FXML
+    public ChoiceBox<String> cmb_preguntas;
+    @FXML
+    public Pane pane_2;
+     
+    
+//    ArrayList<String> items = new ArrayList<>();
+      ObservableList<String> items = FXCollections.observableArrayList();
+  
+
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+            Preguntas();
+
+            cmb_preguntas.setItems(items);
+    }
+    
+    public void Preguntas(){
+        try {
+            stmt = cn.prepareCall("{CALL listar_pre()}");
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                items.add(rs.getString(1));
+            }
+        } catch (Exception e) {
+        }
+    }
     
     public void CheckSecQ(){
-        if (txt_answer.getText().isEmpty() && !txt_User.getText().isEmpty()){
-            //Metodo log in
-            Connection cn = conn.conexion();
+        if (txt_User.getText().length() != 0 && !txt_answer.getText().isEmpty()){
             try {
-                stmt = cn.prepareCall("{select `login`(?, ?, ?)}");
+                stmt = cn.prepareCall("{CALL Sec_Ques(?,?,?)}");
                 stmt.setInt(1, Integer.parseInt(txt_User.getText()));
-                stmt.setInt(2,cmb_Question.getSelectionModel().getSelectedIndex() + 1);     
+                stmt.setInt(2,cmb_preguntas.getSelectionModel().getSelectedIndex() + 1);     
                 stmt.setString(3, txt_answer.getText());
-                //ERROR
-                int result;
+                int result = 0;
                 rs = stmt.executeQuery();
                 while(rs.next()) {
-                    result =rs.getInt(1);
+                    result = rs.getInt(1);
                 }
-                
-                        
+                Enable(result);
+                txt_User.setDisable(true);
             } catch (Exception e) {
             }
-            
         }
     }
     
     public void Enable(int result){
         if (result == 1){
-            lbl_newP.setDisable(false);
-            lbl_confirmP.setDisable(false);
-            txt_Pass1.setDisable(false);
-            txt_Pass2.setDisable(false);
-            btn_save.setDisable(false);
+              pane_2.setDisable(false);
         }
         else {
-            //Mensaje pregunta y respuesta no coincide
+           JOptionPane.showMessageDialog(null, "ERROR", "ERROR", JOptionPane.ERROR_MESSAGE);
         }
     }
     
     public void Save(){
-        
+        if(!txt_Pass1.getText().isEmpty() && !txt_Pass2.getText().isEmpty()){
+            if (txt_Pass1.getText().equals(txt_Pass2.getText())){
+                try {
+                    stmt = cn.prepareCall("{CALL newPass(?,?)}");
+                    stmt.setInt(1, Integer.parseInt(txt_User.getText()));
+                    stmt.setString(2, txt_Pass1.getText());
+                    stmt.execute();
+                } catch (Exception e) {
+                }
+            }
+            else {
+                //Contrasenas no coinciden
+            }
+        }
     }
 }
