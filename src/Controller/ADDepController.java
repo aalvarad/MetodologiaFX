99f5d04430d001f3a6a9a935dbe88d0ab82a3657
mdcv22 <32253690+mdcv22@users.noreply.github.com>
2017-10-31 23:55:39 -0600

    ComboboxDinamico

--- src/Controller/ADDepController.java
+++ src/Controller/ADDepController.java
@@ -6,8 +6,22 @@
 package Controller;
 
 import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -15,16 +29,96 @@ import javafx.fxml.Initializable;
  * @author castrorj
  */
 public class ADDepController implements Initializable {
+    
+    Model.Conection conn = new Model.Conection();
+    CallableStatement stmt;
+    ResultSet rs;
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private AnchorPane anc_Main;
+    @FXML
+    private ImageView img_Teradyne;
+    @FXML
+    private Pane pan_Main;
+    @FXML
+    private Label lbl_BU;
+    @FXML
+    private Label lbl_Department;
+    @FXML
+    private Button btn_Save;
+    @FXML
+    private Label lbl_Area;
+    @FXML
+    private ChoiceBox<String> cmb_Dep;
+    @FXML
+    private Button btn_Act;
+    @FXML
+    private ComboBox<String> cmb_BU;
+    @FXML
+    private ComboBox<String> cmb_area;
+
+
+    ObservableList<String> bus = FXCollections.observableArrayList();
+    ObservableList<String> area = FXCollections.observableArrayList();
+    ObservableList<String> dep = FXCollections.observableArrayList();
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        listar_bu();
+        cmb_BU.setItems(bus);
     }    
     
+    @FXML
     public void DActive () {
         
+    }
+
+    @FXML
+    private void listar_dep(ActionEvent event) {
+            dep.clear();
+          try {
+            Connection cn = conn.conexion();
+            stmt = cn.prepareCall("{CALL listar_Departamento(?)}");
+            stmt.setString(1,cmb_area.getSelectionModel().getSelectedItem());
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                dep.add(rs.getString(1));
+            }
+            cn.close();
+        } catch (Exception e) {
+        }
+        cmb_Dep.setItems(dep);
+        cmb_Dep.setValue(dep.get(0));
+    }
+    
+    
+    public void listar_bu(){
+        try {
+            Connection cn = conn.conexion();
+            stmt = cn.prepareCall("{CALL listar_BU()}");
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                bus.add(rs.getString(1));
+            }
+            cn.close();
+        } catch (Exception e) {
+        }
+    }
+
+    @FXML
+    private void listar_area(ActionEvent event) {
+                area.clear();
+          try {
+            Connection cn = conn.conexion();
+            stmt = cn.prepareCall("{CALL listar_Area(?)}");
+            stmt.setString(1,cmb_BU.getSelectionModel().getSelectedItem());
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                area.add(rs.getString(1));
+            }
+            cn.close();
+        } catch (Exception e) {
+        }
+        cmb_area.setItems(area);
     }
 }
